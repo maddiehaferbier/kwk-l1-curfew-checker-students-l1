d50a95e68f415d668a90428293fa334e6d29aa0a
maddiehaferbier <madisonhaferbier@gmail.com>
2018-06-05 22:38:31 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -13,11 +13,12 @@ def complex_curfew_checker
 end
 
 def deluxe_curfew_checker(time)
-    if time <= 11
-    puts "your late"
-    elsif time > 11
-    puts "late"
-puts "your early"
+    curfew <= 11
+  hours_left ==curfew - time 
+    if time > 11
+    puts "your late. you are #{hours_left} late"
+    elsif time < 11
+puts "your early. you are #{hours_left} hours early"
 end
 end
 deluxe_curfew_checker(12)
